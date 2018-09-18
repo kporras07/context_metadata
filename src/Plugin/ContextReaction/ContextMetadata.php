052e5fb109efac299866514dd3e4f1dc2358c35b
dakku <dakku@97634.no-reply.drupal.org>
2018-09-18 14:54:33 +0100

    PHPCS

--- src/Plugin/ContextReaction/ContextMetadata.php
+++ src/Plugin/ContextReaction/ContextMetadata.php
@@ -19,54 +19,54 @@ class ContextMetadata extends ContextReactionPluginBase {
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $form['metadata_title'] = array(
+    $form['metadata_title'] = [
       '#title' => $this->t('Meta Title'),
       '#description' => $this->t('Title goes here'),
       '#type' => 'textfield',
       '#maxlength' => 256,
       '#default_value' => $this->getConfiguration()['metadata_title'],
-    );
+    ];
 
-    $form['metadata_description'] = array(
+    $form['metadata_description'] = [
       '#title' => $this->t('Meta Description'),
       '#description' => $this->t('Meta Description'),
       '#type' => 'textfield',
       '#maxlength' => 400,
       '#default_value' => $this->getConfiguration()['metadata_description'],
-    );
+    ];
 
-    $form['metadata_keywords'] = array(
+    $form['metadata_keywords'] = [
       '#title' => $this->t('Meta Keywords'),
       '#description' => $this->t('Meta Keywords'),
       '#type' => 'textfield',
       '#maxlength' => 400,
       '#default_value' => $this->getConfiguration()['metadata_keywords'],
-    );
+    ];
 
-    $form['metadata_canonical_url'] = array(
+    $form['metadata_canonical_url'] = [
       '#title' => $this->t('Canonical URL'),
       '#description' => $this->t('Canonical URL'),
       '#type' => 'textfield',
       '#maxlength' => 400,
       '#default_value' => $this->getConfiguration()['metadata_canonical_url'],
-    );
+    ];
 
     // TODO: Add this once we have other metadata working.
     /*$form['metadata_h1_title'] = array(
-      '#title' => $this->t('H1 tag'),
-      '#description' => $this->t('Overrides the H1 title'),
-      '#type' => 'textfield',
-      '#maxlength' => 400,
-      '#default_value' => $this->getConfiguration()['metadata_h1_title'],
+    '#title' => $this->t('H1 tag'),
+    '#description' => $this->t('Overrides the H1 title'),
+    '#type' => 'textfield',
+    '#maxlength' => 400,
+    '#default_value' => $this->getConfiguration()['metadata_h1_title'],
     );*/
 
     // TODO: Add this once we have other metadata working.
     /*$form['metadata_robots'] = array(
-      '#title' => $this->t('Robots'),
-      '#description' => $this->t('Robots'),
-      '#type' => 'textfield',
-      '#maxlength' => 400,
-      '#default_value' => $this->getConfiguration()['metadata_robots'],
+    '#title' => $this->t('Robots'),
+    '#description' => $this->t('Robots'),
+    '#type' => 'textfield',
+    '#maxlength' => 400,
+    '#default_value' => $this->getConfiguration()['metadata_robots'],
     );*/
 
     return $form;
@@ -82,7 +82,7 @@ class ContextMetadata extends ContextReactionPluginBase {
       'metadata_keywords' => $form_state->getValue('metadata_keywords'),
       'metadata_canonical_url' => $form_state->getValue('metadata_canonical_url'),
       // TODO: 'metadata_h1_title' => $form_state->getValue('metadata_h1_title'),
-      // TODO: 'metadata_robots' => $form_state->getValue('metadata_robots'),
+      // TODO: 'metadata_robots' => $form_state->getValue('metadata_robots'),.
     ]);
   }
 
@@ -103,7 +103,7 @@ class ContextMetadata extends ContextReactionPluginBase {
       'metadata_keywords' => '',
       'metadata_canonical_url' => '',
       // TODO: 'metadata_h1_title' => '',
-      // TODO: 'metadata_robots' => '',
+      // TODO: 'metadata_robots' => '',.
     ];
   }
 
